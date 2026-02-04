8ef9d909792a12fe82bebcf523bb641b2b1a8df8
RahulSingh <chouhanrahul1999@gmail.com>
2026-02-05 03:08:57 +0530

    hide the header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,27 @@
-import { Stack } from "expo-router";
-import "./global.css"
 import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+import "./global.css";
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
-    "Rubik-Bold": require('../assets/fonts/Rubik-Bold.ttf'),
-    "Rubik-Regular": require('../assets/fonts/Rubik-Regular.ttf'),
+    "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+    "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
+    "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
   });
-  return <Stack />;
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
