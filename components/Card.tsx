93b3efb63ba8d7604d95ba3a26a7b108db010ac7
RahulSingh <chouhanrahul1999@gmail.com>
2026-02-06 23:41:25 +0530

    created normal card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -28,27 +28,62 @@ export const FeaturedCard = ({ onPress }: Props) => {
       </View>
 
       <View className="flex flex-col items-start absolute bottom-5 inset-x-5">
-        <Text className="text-xl font-rubik-extrabold text-white " numberOfLines={1}>Modern Apartment</Text>
+        <Text
+          className="text-xl font-rubik-extrabold text-white "
+          numberOfLines={1}
+        >
+          Modern Apartment
+        </Text>
 
         <Text className="text-base font-rebik text-white">
-            22 W 15h st, New Yark
+          22 W 15h st, New Yark
         </Text>
 
         <View className="flex flex-row items-center justify-between w-full">
-            <Text className="text-xl font-rubik-extrabold text-white">$1,2500</Text>
+          <Text className="text-xl font-rubik-extrabold text-white">
+            $1,2500
+          </Text>
 
-            <Image source={icons.heart} className="size-5" />
+          <Image source={icons.heart} className="size-5" />
         </View>
       </View>
     </TouchableOpacity>
   );
 };
 
-const Card = () => {
+const Card = ({ onPress }: Props) => {
   return (
-    <View>
-      <Text>Card</Text>
-    </View>
+    <TouchableOpacity
+      onPress={onPress}
+      className="flex-1 w-full mt-4 px-3 py-4 rounded-lg bg-white shadow-lg shadow-black-100 relative"
+    >
+      <View className="flex flex-row items-center absolute px-2 top-5 right-5 bg-white/90 p-1 rounded-full z-50">
+        <Image source={icons.star} className="size-2.5" />
+        <Text className="text-xs font-rubik-bold text-primary-300 ml-0.5">
+          4.4
+        </Text>
+      </View>
+
+      <Image source={images.newYork} className="w-full h-40 rounded-lg" />
+
+      <View className="flex flex-col mt-2">
+        <Text className="text-base font-rubik-bold text-black-300 ">
+          Cozy Studio
+        </Text>
+
+        <Text className="text-xs font-rebik text-black-200">
+          22 W 15h st, New Yark
+        </Text>
+
+        <View className="flex flex-row items-center justify-between mt-2">
+          <Text className="text-base font-rubik-bold text-primary-300">
+            $1,2500
+          </Text>
+
+          <Image source={icons.heart} className="w-5 h-5 mr-2" tintColor="#191d31" />
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 };
 
