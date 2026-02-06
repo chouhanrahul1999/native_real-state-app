34b5484590eda07152122e9bf012e8e2b522ef5c
RahulSingh <chouhanrahul1999@gmail.com>
2026-02-07 00:04:25 +0530

    created the ui of filters

---
+++ components/Filters.tsx
@@ -0,0 +1,39 @@
+import { View, Text, ScrollView, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { useLocalSearchParams } from "expo-router";
+import { categories } from "@/constants/data";
+
+const Filters = () => {
+  const params = useLocalSearchParams<{ Filter?: string }>();
+  const [selectedCatagory, setSelectedCatagory] = useState(
+    params.Filter || "All",
+  );
+
+  const handleCatagory = (catagory: string) => {
+    setSelectedCatagory(catagory);
+  };
+
+  return (
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      className="mt-3 mb-2"
+    >
+      {categories.map((item, index) => (
+        <TouchableOpacity
+          key={index}
+          className={`flex flex-col items-start mr-4 px-4 py-2 rounded-full ${selectedCatagory === item.category ? "bg-primary-300" : "bg-primary-100 border border-primary-200"}`}
+          onPress={() => handleCatagory(item.category)}
+        >
+          <Text
+            className={`text-sm ${selectedCatagory === item.category ? "text-white font-rubik-bold mt-0.5" : "text-black-300 font-rubik"}`}
+          >
+            {item.title}
+          </Text>
+        </TouchableOpacity>
+      ))}
+    </ScrollView>
+  );
+};
+
+export default Filters;
