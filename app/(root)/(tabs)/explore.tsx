37db965de1b4c83bc26a435b00a0ce9afc6154bb
RahulSingh <chouhanrahul1999@gmail.com>
2026-02-08 17:23:42 +0530

    completed the explore page

--- app/(root)/(tabs)/explore.tsx
+++ app/(root)/(tabs)/explore.tsx
@@ -1,12 +1,100 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { getLatestProperties, getProperties } from "@/app/lib/appwite";
+import { userGlobalContext } from "@/app/lib/global-provider";
+import { useAppwrite } from "@/app/lib/useAppwrite";
+import Card, { FeaturedCard } from "@/components/Card";
+import Filters from "@/components/Filters";
+import NoResult from "@/components/NoResult";
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { router, useLocalSearchParams } from "expo-router";
+import { useEffect } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const Explore = () => {
+export default function Explore() {
+  const params = useLocalSearchParams<{ query?: string; filter?: string }>();
+
+  const {
+    data: properties,
+    loading,
+    refetch,
+  } = useAppwrite({
+    fn: getProperties,
+    params: {
+      query: params.query || "",
+      filter: params.filter || "All",
+      limit: 6,
+    },
+    skip: true,
+  });
+
+  const handleCardPress = (id: string) => {
+    router.push(`/property/${id}` as any);
+  };
+
+  useEffect(() => {
+    refetch({
+      filter: params.filter || "All",
+      query: params.query || "",
+      limit: 20,
+    });
+  }, [params.filter, params.query]);
+
   return (
-    <View>
-      <Text>Explore</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-white h-full">
+      <FlatList
+        data={properties}
+        renderItem={({ item }) => (
+          <Card item={item} onPress={() => handleCardPress(item.$id)} />
+        )}
+        keyExtractor={(item) => item.$id}
+        numColumns={2}
+        contentContainerClassName="pb-32"
+        columnWrapperClassName="flex gap-5 px-5"
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={
+          loading ? (
+            <ActivityIndicator size="large" className="text-primary-300 mt-5" />
+          ) : (
+            <NoResult />
+          )
+        }
+        ListHeaderComponent={
+          <View className="mx-5">
+            <View className="flex flex-row items-center justify-between mt-5">
+              <TouchableOpacity
+                onPress={() => router.back()}
+                className="flex flex-row bg-primary-200 rounded-full size-11 items-center justify-center"
+              >
+                <Image source={icons.backArrow} className="size-5" />
+              </TouchableOpacity>
 
-export default Explore
+              <Text className="text-base mr-2 text-center font-rubik-medium text-black-300">
+                Search for Your Ideal Home
+              </Text>
+
+              <Image source={icons.bell} className="w-6 h-6" />
+            </View>
+            <Search />
+
+            <View className="mt-5">
+              <Filters />
+
+              <Text className="text-xl font-rubik-bold text-black-300 mt-5">
+                Found {properties?.length} Propertes
+              </Text>
+            </View>
+          </View>
+        }
+      />
+    </SafeAreaView>
+  );
+}
